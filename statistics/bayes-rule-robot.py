c65cd1710d1fa44f3729aa95f75e65a47dd65b9b
aemonge <andres@aemonge.com>
2022-09-22 21:53:01 +0200

    statistics(bayees-rule): robot sensing 4

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 msg = """
+A: Red   Box
+B: Green Box
+C" Green Box
 
-P(A∣B)= ( P(B∣A)⋅P(A) ) / P(B)
-
-Robot Lives in G: Green and R: Red
-    P(R)                   = {}
-    P(G)                   = {}
-    see Red   P(sR | at R) = {}
-    see Green P(sG | at G) = {}
-    P(R | sR)             >= {}
-    P(G | sR)             >= {}
+Robot Lives in one box
+    P(A)                    = {}
+    P(B)                    = {}
+    P(C)                    = {}
     ------
-    P(sR)                  = P(R) * P(sR|R) + P(G) * P(sR|G)
+    P(R|A) ~see red in A~   = {}
+    P(G|B) ~see green in B~ = {}
+    P(G|C) ~see green in C~ = {}
 """
-red, green = .5, .5
-see_red_at_red, see_green_at_green= .8, .5
-# ---------------
-at_red_see_red   = red * see_red_at_red
-at_green_see_red = green * (1 - see_green_at_green)
-zum = sum([at_green_see_red, at_red_see_red])
-# ------ normalize
-at_red_see_red   /= zum
-at_green_see_red /= zum
+
+ans= """
+    ----------------------------
+    P(A,R)                  = {}
+"""
+red, green1, green2 = .3333, .3333, .3333
+see_red_a, see_green_b, see_green_c =  .9, .9, .9
 print(msg.format(
-    round(red, 4), round(green, 4), round(see_red_at_red, 4), round(see_green_at_green, 4),
-    # ------ Results
-    round(at_red_see_red, 4), round(at_green_see_red, 4)))
+    round(red, 4), round(green1, 4), round(green2, 4),
+    round(see_red_a, 4), round(see_green_b, 4), round(see_green_c, 4)
+))
+# ---------------
+a_see_red = (red * see_red_a)
+print(ans.format(
+    round(a_see_red, 4)
+))

fdae28d262ec7418e965d7473b37d885072beb6b
aemonge <andres@aemonge.com>
2022-09-22 21:42:59 +0200

    statistics(bayees-rule): robot sensing 3

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -4,21 +4,25 @@ msg = """
 P(A∣B)= ( P(B∣A)⋅P(A) ) / P(B)
 
 Robot Lives in G: Green and R: Red
-    P(R)                   = P(G) = 0.5
-    see Red   P(sR | at R) = 0.8
-    see Green P(sG | at G) = 0.8
+    P(R)                   = {}
+    P(G)                   = {}
+    see Red   P(sR | at R) = {}
+    see Green P(sG | at G) = {}
     P(R | sR)             >= {}
     P(G | sR)             >= {}
     ------
     P(sR)                  = P(R) * P(sR|R) + P(G) * P(sR|G)
 """
-red, green = 0, .1
-see_red_at_red, see_greenat_green= .8, .8
+red, green = .5, .5
+see_red_at_red, see_green_at_green= .8, .5
 # ---------------
-see_red_at_green = 1 - see_red_at_red
-see_red = red * see_red_at_red + green * see_red_at_green
-# ---------------
-at_red_see_green = (see_red_at_red * (red) / see_red)
-at_green_see_red = (1 - at_red_see_green) # + (green)
-
-print(msg.format(round(at_red_see_green, 5), round(at_green_see_red, 5)))
+at_red_see_red   = red * see_red_at_red
+at_green_see_red = green * (1 - see_green_at_green)
+zum = sum([at_green_see_red, at_red_see_red])
+# ------ normalize
+at_red_see_red   /= zum
+at_green_see_red /= zum
+print(msg.format(
+    round(red, 4), round(green, 4), round(see_red_at_red, 4), round(see_green_at_green, 4),
+    # ------ Results
+    round(at_red_see_red, 4), round(at_green_see_red, 4)))

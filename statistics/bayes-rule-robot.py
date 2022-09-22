9b82e5611a6e426130bdd23f0a9f8e52f0ac9b2f
aemonge <andres@aemonge.com>
2022-09-22 21:55:10 +0200

    statistics(bayees-rule): robot sensing 5

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -17,15 +17,17 @@ Robot Lives in one box
 ans= """
     ----------------------------
     P(A,R)                  = {}
+    P(B,R)                  = {}
 """
-red, green1, green2 = .3333, .3333, .3333
+a, b, c = .3333, .3333, .3333
 see_red_a, see_green_b, see_green_c =  .9, .9, .9
 print(msg.format(
-    round(red, 4), round(green1, 4), round(green2, 4),
+    round(a, 4), round(b, 4), round(c, 4),
     round(see_red_a, 4), round(see_green_b, 4), round(see_green_c, 4)
 ))
 # ---------------
-a_see_red = (red * see_red_a)
+a_see_red = (a * see_red_a)
+b_see_red = (b * (1 - see_green_b))
 print(ans.format(
-    round(a_see_red, 4)
+    round(a_see_red, 4), round(b_see_red, 4)
 ))

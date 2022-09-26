cbc75113f11858810ad5b80df3a06f2bc5437584
aemonge <andres@aemonge.com>
2022-09-26 14:52:19 +0200

    statistics(bayees-rule-pyton): 8-Program Flipping

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -1,28 +1,9 @@
 #!/usr/bin/python3
-msg="""
-Sebastian travels so much, he can't remember where he is
-    P(away)      = {}
-    P(home)      = {}
-    P(rain|home) = {}
-    P(rain|away) = {}
-"""
-away, home = .6, .4
-rains_home  = .01
-rains_away  = .3
 
-print(msg.format(away, home, rains_home, rains_away))
+def f(p0, p1, p2):
+    """P(H|c1) = p1"""
+    """P(H|c2) = p2"""
 
-ans="""
-  Sebastian wakes up and it's raining
-    P(home|rain) = {}
-"""
+    return (p0*p1 + (1 -p0)*p2)
 
-mid="""
-    P(home, rain) =
-
-"""
-home_raining = home * rains_home
-away_raining = away * rains_away
-zum = sum([away_raining, home_raining])
-
-print(ans.format(home_raining/zum))
+print(f(.3, .5, .9))

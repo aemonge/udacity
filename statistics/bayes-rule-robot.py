288d2576d8757f2effdce0bbe88c65b5ef34a30a
aemonge <andres@aemonge.com>
2022-09-26 15:10:54 +0200

    statistics(bayees-rule-pyton): 11-Cancer example 3

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+#Return the probability of A conditioned on Not B given that
+# P(A|~B) = P(A)*P(B|A) / (P(A)*P(B|A)) + (1-P(A) * )
+#P(A)=p0, P(B|A)=p1, and P(Not B|Not A)=p2
 
-def f(p0, p1, p2):
-    """P(C) = P(C)*P(Pos|C) + P(C)*P(Neg|C)"""
-    """P(C|Pos) = P(C)*P(Pos|C) + """
-    return p0*p1 / (p0*p1 + (1-p0)*(1-p2))
+def f(p0,p1,p2):
+#Insert your code here
+    return p0*p1 / ( (p0*p1) + (1-p0)*p2 )
 
 print(f(.1, .9, .8))

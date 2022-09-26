a5353abe8c29367bb6f570be12c0bfd73603c294
aemonge <andres@aemonge.com>
2022-09-26 14:54:56 +0200

    statistics(bayees-rule-pyton): 10-Cancer example

--- statistics/bayes-rule-robot.py
+++ statistics/bayes-rule-robot.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def f(p0, p1, p2):
-    """P(H|c1) = p1"""
-    """P(H|c2) = p2"""
+    return p1*p0 + (1-p0)*(1-p2)
 
-    return (p0*p1 + (1 -p0)*p2)
-
-print(f(.3, .5, .9))
+print(f(.1, .9, .8))

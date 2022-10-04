5892110fd6440cebf4ea51baa4fb6e73f55e3714
aemonge <andres@aemonge.com>
2022-10-04 21:17:59 +0200

    python(package): Making a package

--- gaussian-package/Blouse.py
+++ gaussian-package/Blouse.py
@@ -5,36 +5,36 @@ class Clothing:
         self.size = size
         self.style = style
         self.price = price
-        
+
     def change_price(self, price):
         self.price = price
 
     def calculate_shipping(self, weight, rate):
         return weight*rate;
-        
+
     def calculate_discount(self, discount):
         return self.price * (1 - discount)
-        
+
 class Shirt(Clothing):
-    
+
     def __init__(self, color, size, style, price, long_or_short):
-        
+
         Clothing.__init__(self, color, size, style, price)
         self.long_or_short = long_or_short
-    
+
     def double_price(self):
         self.price = 2*self.price
-    
+
 class Pants(Clothing):
 
     def __init__(self, color, size, style, price, waist):
-        
+
         Clothing.__init__(self, color, size, style, price)
         self.waist = waist
-        
+
     def calculate_discount(self, discount):
         return self.price * (1 - discount / 2)
-    
+
 class Blouse(Clothing):
     def __init__(self, color, size, style, price, country_of_origin):
         Clothing.__init__(self, color, size, style, price)
@@ -46,7 +46,7 @@ class Blouse(Clothing):
 # TODO: Write a class called Blouse, that inherits from the Clothing class
 # and has the the following attributes and methods:
 #   attributes: color, size, style, price, country_of_origin
-#     where country_of_origin is a string that holds the name of a 
+#     where country_of_origin is a string that holds the name of a
 #     country
 #
 #   methods: triple_price, which has no inputs and returns three times

674f00adc3d2851f0e430553b7b926c47b9c3dad
aemonge <andres@aemonge.com>
2022-11-03 10:26:32 +0100

    seaborn(101): (day 2) Just doing the excersice from the videos, 🐟 are starting to click to me

--- introduction-pAIthon-devs/seaborn/pokemon-pie.py
+++ introduction-pAIthon-devs/seaborn/pokemon-pie.py
@@ -1,3 +1,4 @@
+
 %%html
 <script>
     // AUTORUN ALL CELLS ON NOTEBOOK-LOAD!
@@ -12,6 +13,7 @@
         }
     );
 </script>
+
 # |%%--%%| <mZ4hT70dFv|VEQ5NNc7W6>
 """°°°
 # Pokemon with Pie/Donut charts
@@ -38,14 +40,72 @@ inc = 3
 bins = np.arange(pokemon['speed'].min(), pokemon['speed'].max() + inc, inc)
 plt.hist(data = pokemon, x = 'speed', bins = bins)
 
-#|%%--%%| <TCjxPNNUdI|v2l8llXOqc>
+# |%%--%%| <TCjxPNNUdI|v2l8llXOqc>
 """°°°
-# Rank the pokemons by speed with a speed check of `10`
+# Rank the pokemons by speed with a speed check of `10
 ## **but** ✳️  using seaborn and not matplotlib.
 °°°"""
-#|%%--%%| <v2l8llXOqc|ELJt0iOVBm>
+# |%%--%%| <v2l8llXOqc|ELJt0iOVBm>
 
 inc = 10
 bins = np.arange(pokemon['speed'].min(), pokemon['speed'].max() + inc, inc)
 sb.displot(pokemon['speed'], kde = True) # kde = False -> To remove the line
+#|%%--%%| <ELJt0iOVBm|kqTeQmvxAc>
+"""°°°
+# Sub plots
+°°°"""
+#|%%--%%| <kqTeQmvxAc|PAKl1fGhYa>
+
+# Resize the chart, and have two plots side-by-side
+# set a larger figure size for subplots
+plt.figure(figsize = [20, 5])
+
+# histogram on left, example of too-large bin size
+# 1 row, 2 cols, subplot 1
+plt.subplot(1, 2, 1)
+bins = np.arange(0, pokemon['speed'].max()+4, 4)
+plt.hist(data = pokemon, x = 'speed', bins = bins);
+
+# histogram on right, example of too-small bin size
+plt.subplot(1, 2, 2) # 1 row, 2 cols, subplot 2
+bins = np.arange(0, pokemon['speed'].max()+1/4, 1/4)
+plt.hist(data = pokemon, x = 'speed', bins = bins);
+
+#|%%--%%| <PAKl1fGhYa|TXBIVvW3dJ>
+"""°°°
+## What if we remove the subplot
+°°°"""
+#|%%--%%| <TXBIVvW3dJ|FgBLUtWBMQ>
+
+# Resize the chart, and have two plots side-by-side
+# set a larger figure size for subplots
+plt.figure(figsize = [20, 5])
+
+# histogram on left, example of too-large bin size
+# 1 row, 2 cols, subplot 1
+bins = np.arange(0, pokemon['speed'].max()+4, 4)
+plt.hist(data = pokemon, x = 'speed', bins = bins);
+
+# histogram on right, example of too-small bin size
+bins = np.arange(0, pokemon['speed'].max()+1/4, 1/4)
+plt.hist(data = pokemon, x = 'speed', bins = bins);
+
+#|%%--%%| <FgBLUtWBMQ|oSP3cxGtMO>
+"""°°°
+## Spacing out the data
+°°°"""
+#|%%--%%| <oSP3cxGtMO|8FnucGvSFw>
+
+# Resize the chart, and have two plots side-by-side
+# set a larger figure size for subplots
+plt.figure(figsize = [20, 5])
+
+# histogram on left, example of too-large bin size
+# 1 row, 2 cols, subplot 1
+bins = np.arange(0, pokemon['speed'].max()+4, 4)
+plt.hist(data = pokemon, x = 'speed', bins = bins, rwidth=0.7);
+
+# histogram on right, example of too-small bin size
+bins = np.arange(0, pokemon['speed'].max()+1/4, 1/4)
+plt.hist(data = pokemon, x = 'speed', bins = bins, rwidth=0.7);
 
